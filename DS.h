8e9dc5736a47af1ef445d785ec5015e75630746b
Felanthios <99811138+Felanthios@users.noreply.github.com>
2022-05-19 19:04:12 +0800

    Update DS.h

--- DS.h
+++ DS.h
@@ -1,16 +1,22 @@
 #ifndef __DOUBLE_LINKED_LIST
 #define __DOUBLE_LINKED_LIST
 
-typedef struct Node {
-    void *data;
-    struct Node *prev;
-    struct Node *next;
-} *NodePointer;
+struct linked_list {
+	int data;
+    struct linked_list *prev;
+	struct linked_list *next;
+};
 
-void InsertHead(NodePointer *head_pointer, void *data);
-void InsertTail(NodePointer *head_pointer, void *data);
-void print(NodePointer *head_pointer);
-NodePointer SearchNode(NodePointer *head_pointer, void *data);
-void DeleteNodeByData(NodePointer *head_pointer, void *data);
+typedef struct linked_list node;
+node *front = NULL;
+node *end = NULL;
+void create();
+void insert_at_first(int);
+void insert_at_last(int);
+void insert_at_position(int, int);
+void delete_node(int);
+void search(int);
+void forward_traverse();
+void backward_traverse();
 
 #endif
