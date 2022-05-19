4bdd754a8a239718414719e59e11dfd848b4a7a9
Felanthios <99811138+Felanthios@users.noreply.github.com>
2022-05-19 18:58:45 +0800

    Update Doubly_linked_list.c

--- Doubly_linked_list.c
+++ Doubly_linked_list.c
@@ -1,74 +1,138 @@
-#include <stdio.h>
-#include <stdlib.h>
+void create() {
+    int value;
 
-#include "DS.h"
+    while(1) {
+        printf("Input a value, Enter -1 to exit\n");
+        scanf("%d", &value);
 
-void InsertHead(NodePointer *head_pointer, void *data) {
-    NodePointer new_node = (NodePointer) malloc(sizeof(struct Node));
-    new_node -> data = data;
-    if (*head_pointer == NULL) {
-        new_node -> prev = NULL;
-        new_node -> next = NULL;
-        return;  
-    }
-    
-    (*head_pointer) -> prev = new_node;
-    new_node -> next = *head_pointer;
-    new_node -> prev = NULL;
-    *head_pointer = new_node;
-}
+        if (value == -1) {
+            break;
+        }
 
-void InsertTail(NodePointer *head_pointer, void *data) {
-    
-    NodePointer new_node = (NodePointer) malloc(sizeof(struct Node));
-    new_node -> data = data;
-    new_node -> next = NULL;
-    NodePointer cur_node = *head_pointer;
-    
-    if (*head_pointer == NULL) {
-        new_node -> prev = NULL;
-        *head_pointer = new_node;
-        return;
-    }
-    
-    while (cur_node -> next != NULL)
-        cur_node = cur_node -> next;
-    
-    cur_node -> next = new_node;
-    new_node -> prev = cur_node;
-}
-
-void Print(NodePointer *head_pointer) {
-    if (*head_pointer == NULL)
-        return;
-    NodePointer cur_node = *head_pointer;
-    printf("%d", *(int*)(cur_node -> data));
-    while (cur_node -> next != NULL) {
-        cur_node = cur_node -> next;
-        printf("%d", *(int*)(cur_node -> data));
+        insert_at_last(value);
     }
 }
 
-NodePointer SearchNode(NodePointer *head_pointer, void *data) {
-    NodePointer cur_node = *head_pointer;
-    while (cur_node != NULL && cur_node -> data != data) {
-        cur_node = cur_node -> next;
-    }
-    return cur_node;
+void insert_at_first(int value) {
+	node *temp;
+	temp = (node*)malloc(sizeof(node));
+	temp->data = value;
+	temp->prev = NULL;
+	temp->next = front;
+
+	if(front == NULL) {
+		end = temp;
+	} else {
+		front->prev = temp;
+	}
+
+	front = temp;
 }
 
-void DeleteNodeByData(NodePointer *head_pointer, void *data) {
-    NodePointer node = SearchNode(head_pointer, data);
-    if (node == NULL)
-        return;
-    if (*head_pointer == node) {
-        *head_pointer = node -> next;
+void insert_at_last(int value) {
+	node *temp;
+	temp = (node*)malloc(sizeof(node));
+	temp->data = value;
+	temp->prev = end;
+	temp->next = NULL;
+
+	if(end == NULL) {
+		front = temp;
+	} else {
+		end->next = temp;
+	}
+
+	end = temp;
+}
+
+void insert_at_position(int position, int value) {
+	node *temp_node = front;
+	node *new_node = (node*)malloc(sizeof(node));
+	new_node->data = value;
+
+	for (int i = 0; i < (position - 1); i++) {
+		temp_node = temp_node->next;
+	}
+
+	if (temp_node == NULL) {
+		printf("\nThis position is too far\n");
+		return;
+	}
+
+
+	new_node->next = temp_node->next;
+	new_node->prev = temp_node;
+	temp_node->next = new_node;
+}
+
+void search(int value) {
+    node *temp_node = front;
+    int flag = 0;
+
+    while (temp_node != NULL) {
+        if (temp_node->data == value) {
+            printf("%d is present in this list. Memory address is %p\n", value, temp_node);
+            flag = 1;
+            break;
+        } else {
+            temp_node = temp_node->next;
+        }
     }
-    if (node -> next != NULL) {
-        node -> next -> prev = node -> prev;
+
+    if (flag == 0) {
+        printf("Item not found\n");
     }
-    if (node -> prev != NULL) {
-        node -> prev -> next = node -> next;
+}
+
+void delete_node(int value) {
+	node *temp_node = front;
+	int flag = 0;
+
+    while (temp_node != NULL) {
+        if (temp_node->data == value) {
+            if (temp_node->prev == NULL) {
+                front = temp_node->next;
+                front->prev = NULL;
+            } else if (temp_node->next == NULL) {
+            	end = temp_node->prev;
+            	end->next = NULL;
+            } else {
+            	temp_node->prev->next = temp_node->next;
+            	temp_node->next->prev = temp_node->prev;
+            }
+
+            free(temp_node);
+            printf("%d is deleted from list\n", value);
+            flag = 1;
+            break;
+        }
+
+        temp_node = temp_node->next;
     }
-    free(node);
+
+    if (flag == 0) {
+        printf("%d is not in the list!\n", value);
+    }
+}
+
+void forward_traverse() {
+	node *temp = front;
+
+	while(temp != NULL) {
+		printf("%d ", temp->data);
+		temp = temp->next;
+	}
+
+	printf("\n");
+}
+
+void backward_traverse() {
+	node *temp = end;
+
+	while(temp != NULL) {
+		printf("%d ", temp->data);
+		temp = temp->prev;
+	}
+
+	printf("\n");
 }
